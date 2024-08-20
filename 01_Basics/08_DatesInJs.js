75ca68968702296b9f2f196b78c274e958806a04
Adarsh-1010 <adarsh.21ug3009@iiitranchi.ac.in>
2024-08-21 01:52:50 +0530

    Date and time

---
+++ 01_Basics/08_DatesInJs.js
@@ -0,0 +1,13 @@
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+//Another Method of Declaring Dates
+
+let myCreatedDates=new Date(2023,0,23);
+console.log(myCreatedDates.toDateString());
+
+let myCreatedDates1=new Date(2023,0,23,5,3);
+console.log(myCreatedDates1.toLocaleString());
+
